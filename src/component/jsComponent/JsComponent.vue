811dde96a9574aa7324b0689933e1621de1f1ec8
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-06-27 09:01:23 +0900

    feat:JSとTSのコンポーネントを用意

---
+++ src/component/jsComponent/JsComponent.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  .js-component
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+
+</style>
+
+
---
+++ src/component/tsComponent/TsComponent.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  .ts-component
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator"
+
+@Component({})
+export default class TsComponent extends Vue {}
+</script>
+
+<style lang="scss">
+
+</style>
