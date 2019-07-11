3e7678f2e2a4f48e931fb5ab106ea817058c37bf
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-07-11 16:03:32 +0900

    デモ用の実装

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,24 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from "vue"
+import Vuex from "vuex"
 
-Vue.use(Vuex);
+Vue.use(Vuex)
+
+interface State {
+  value: string
+}
 
 export default new Vuex.Store({
   state: {
-
+    value: ""
+  },
+  getters: {
+    getValue(state: State): string {
+      return state.value
+    }
   },
   mutations: {
-
-  },
-  actions: {
-
-  },
-});
+    setValue(state: State, payload: string): void {
+      state.value = payload
+    }
+  }
+})
---
+++ src/views/child.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  .child Child Component 
+    button(@click="$emit('childEmitEvent')") Click Here!!!
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator"
+import Store from "@/store"
+
+@Component({})
+export default class Child extends Vue {
+  public created(): void {
+    Store.commit("setValue", "hoge")
+    console.log(Store.getters.getValue)
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
